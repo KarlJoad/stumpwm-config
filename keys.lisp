7103bfc90251094edcdc9de519e7c38979093879
Karl Hallsby <karl@hallsby.com>
2022-03-15 22:25:11 -0500

    Start nested keymap for emacs stuff

--- keys.lisp
+++ keys.lisp
@@ -15,3 +15,12 @@
 (define-key *root-map* (kbd "c") "exec xfce4-terminal")
 (define-key *root-map* (kbd "C-c") "exec xfce4-terminal")
 (define-key *root-map* (kbd "z") "exec nyxt")
+
+;; My Personal Emacs keybindings
+(defvar *emacs-keybindings*
+  (let ((m (make-sparse-keymap)))
+    (define-key m (kbd "e") "exec emacsclient -s server -c")
+    (define-key m (kbd "m") "exec emacsclient -s server -c -e (mu4e)")
+    m ; NOTE: Important to return to make final value of let-binding be keymap
+    ))
+(define-key *root-map* (kbd "e") '*emacs-keybindings*)
